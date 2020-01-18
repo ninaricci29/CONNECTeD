089b0dcd4cf6f87f3caa383b8826da56374ad891
VeerpalBrar <v.brar@mail.utoronto.ca>
2020-01-18 14:26:29 -0500

    add router plugin

---
+++ client/src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }, 
+  {
+    path: '/register',
+    name: 'register',
+    component: () => 
+      import(/* webpackChunkName: "register"*/ '../views/Register.vue')
+  }, 
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => 
+      import(/* webpackChunkName: "register"*/ '../views/Profile.vue')
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: "/connect",
+  routes
+});
+
+export default router;
---
+++ client/src/views/Profile.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an profile page</h1>
+  </div>
+</template>
