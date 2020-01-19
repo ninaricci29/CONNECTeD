5ee7ce6ab40b078dac0f91ebc82c0f99c220353c
marissa <marissa.shey@mail.utoronto.ca>
2020-01-18 21:39:42 -0500

    merged task2 dir with vue

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an profile page</h1>
+  <div class="container">
+    <div class="card card-widget widget-user">
+    <div class="widget-user-header text-white" >
+      <h3 class="username"> {{name}} </h3>
+
+
+    </div>
+    </div>
   </div>
+
 </template>
+<script>
+  export default {
+    name:"profile",
+    data() {
+      return {
+        name: 'Jane Doe',
+        desc: 'I like to code!'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body {background-color: powderblue;}
+  h1   {color: blue;}
+  p    {color: red;}
+</style>
+
