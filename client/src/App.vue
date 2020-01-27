3b9e9d09c829be39287444e2ba2ca3791ac92f6a
Akarshi <akarshi.sharma@mail.utoronto.ca>
2020-01-27 13:58:54 -0500

    Nav Bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/register">Register</router-link> |
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <b-navbar toggleable="lg" type="light" variant="info">
+    <b-navbar-brand href="#">ConnectED</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/home" > Home </b-nav-item>
+        <b-nav-item to="/register">Register </b-nav-item>
+        <b-nav-item to="/profile"> Profile </b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item to='/profile'>Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
     <router-view />
-  </div>
+</div>
 </template>
 
 <style>
@@ -29,6 +52,9 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgb(0, 0, 140);
+}
+div{
+  text-decoration-color: black;
 }
 </style>
