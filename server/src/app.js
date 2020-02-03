6556df686d0cde77847be3e0cd48f8a51414709c
VeerpalBrar <v.brar@mail.utoronto.ca>
2020-02-03 09:56:37 -0500

    serve static files in express

--- server/src/app.js
+++ server/src/app.js
@@ -5,13 +5,18 @@ const cors = require('cors')
 const models = require('./models')
 const app = express()
 const config = require("./config/config")
+const serveStatic = require('serve-static')
+const path = require('path')
+var history = require('connect-history-api-fallback');
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cors())
-
+//app.use(serveStatic(path.join(__dirname,'/dist')))
+app.use('/connect', express.static(path.join(__dirname, 'dist')))
+console.log(`${path.join(__dirname, "/dist")}`)
 require('./routes')(app)
-
+app.use(history())
 models.sequelize.sync()
   .then( () => {
     app.listen(config.port)
