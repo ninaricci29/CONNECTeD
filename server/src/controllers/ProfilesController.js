253f741ff74e1dc213f3cecfe465d0b623d6e162
ishaaa-ai <56453971+ishaaa-ai@users.noreply.github.com>
2020-02-05 16:47:22 -0500

    Update profile information (limited to back-end)

--- server/src/controllers/ProfilesController.js
+++ server/src/controllers/ProfilesController.js
@@ -27,5 +27,29 @@ module.exports = {
         error: 'an error has occurred trying to register the user'
       })
     }
+  },
+
+  async update_profile (req, res){
+    try{
+      console.log(req)
+      const user = await User.findOne({where: {
+        id: req.body.id
+      }})
+      user.utorid=req.body.utorid;
+      user.first_name=req.body.first_name;
+      user.last_name=req.body.last_name;
+      user.bio=req.body.bio;
+      user.major=req.body.major;
+      user.year=req.body.year;
+      user.description=req.body.description;
+      await user.save();
+      res.send("succesfully updated")
+    } 
+    catch (err) {
+      console.log(err);
+      res.status(500).send({
+        error: 'an error has occurred trying to update the user'
+      })
+    }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,5 +2,6 @@ const ProfileController = require('./controllers/ProfilesController')
 
 module.exports = (app) => {
   app.get('/profile', ProfileController.retrieve), 
-  app.post('/profile', ProfileController.register);
+  app.post('/profile', ProfileController.register),
+  app.post('/updateprofile', ProfileController.update_profile)
 }
