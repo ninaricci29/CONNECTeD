bb96dba01b71ba3e601def3506da8b59e1b47fb2
ninaricci <itsninaricci.lu@mail.utoronto.ca>
2020-02-20 12:20:30 -0500

    Removed unnecessary lines, comments, and fixed master.

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -3,44 +3,22 @@
         <ProfileHeader/>
         <Projects class="profile-projects"/>
     </div>
-
 </template>
 
 <script>
-    import ProfileHeader from "@/components/ProfileHeader.vue";
+    import ProfileHeader from "../components/ProfileHeader.vue";
     import Projects from "../components/Projects";
-    // import axios from "axios";
     export default {
         name: "profile",
         components: {
             ProfileHeader,
             Projects
-        },
-        data() {
-            return {
-                name,
-                description: null,
-                year: null,
-                major: null,
-                rating: null
-            }
-        },
-        mounted() {
-            axios.get('http://localhost:8081/profile?id=2')
-                .then(response => (
-                    this.name = response.data.first_name + ' ' + response.data.last_name,
-                    this.description = response.data.bio,
-                    this.year = response.data.year,
-                    this.major = response.data.major
-                ));
-
         }
     }
 </script>
 
-<style>
+<style scoped>
     .profile-projects {
         padding-top: 80px;
     }
-
 </style>
