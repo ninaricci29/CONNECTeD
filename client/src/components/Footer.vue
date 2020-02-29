d7c92e7e10257dd8eb97f7aa0ee4d00f3a5ee5cd
ninaricci <itsninaricci.lu@mail.utoronto.ca>
2020-02-28 23:18:39 -0500

    created a basic footer

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="footer">
+        <div class="inner-footer">
+            <div class="logo-container info">
+                ConnectEd
+            </div>
+
+            <div>
+                <div class="info-inner">
+                    <li><a> About </a></li>
+                </div>
+
+                <b-row  class="info-inner">
+                    <li>
+                        <div class="text-muted"> Have an account? </div>
+                    </li>
+
+                    <li>
+                        <a>
+                            <b-link href="/log-in"> Sign In </b-link>
+                        </a>
+                    </li>
+                </b-row>
+
+            </div>
+
+            <div class="info-inner">
+                &copy; {{ new Date().getFullYear() }} NanoBanano Co.
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"footer"
+
+    };
+</script>
+
+<style scoped>
+    * {
+        list-style: none;
+        text-decoration: none;
+    }
+
+    .footer {
+        /*width: 100vw;*/
+        display: block;
+        overflow: hidden;
+        /*box-sizing: border-box;*/
+        background-color: #18181a;
+        color: white;
+    }
+
+    .inner-footer {
+        display: block;
+        text-align: left;
+        /*width: 1100px;*/
+        height: 100%;
+        margin: 40px;
+
+    }
+
+    .logo-container {
+        font-weight: bold;
+        font-size: 22px;
+    }
+
+    .info {
+        margin: 10px 20px 20px 120px;
+    }
+
+    .info-inner {
+        font-size: 14px;
+        margin: 10px 0 0 120px;
+        padding-bottom: 0px;
+
+    }
+
+    .info-inner li:not(:first-child) {
+        padding-left: 20px;
+    }
+
+    .info-inner li a {
+        text-decoration: none;
+        color: white;
+    }
+
+    .info-inner li a:hover{
+        cursor: pointer;
+
+    }
+
+</style>
