4671d96e95cb7f751cdd1d6ea5f32f83a4906369
ninaricci <itsninaricci.lu@mail.utoronto.ca>
2020-03-11 00:56:39 -0400

    fixed error

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,20 +1,3 @@
 module.exports = {
   publicPath: "/connect"
 };
-
-const GoogleFontsPlugin = require("google-fonts-webpack-plugin")
-
-module.exports = {
-  "entry": "index.js",
-  /* ... */
-  plugins: [
-    new GoogleFontsPlugin({
-      fonts: [
-        //  { family: "Raleway", variants: [ "400", "700italic" ] }
-        { family: "Montserrat" },
-        { family: "Raleway" },
-        { family: "Poppins" },
-      ]
-    })
-  ]
-}
